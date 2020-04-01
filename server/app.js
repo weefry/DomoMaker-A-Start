765ae39ddb5d6b985ec348c5773c04a2ed45ba6d
Nicholas Jones <nxj8268@g.rit.edu>
2020-04-01 18:44:58 -0400

    domomaker

---
+++ server/app.js
@@ -0,0 +1,52 @@
+// import libs
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressHandlebars = require('express-handlebars');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = process.env.MONGODB_URI || 'mongodb://heroku_1cq9q2t5:jjf57lsibk83frfo2a11fle6fa@ds125616.mlab.com:25616/heroku_1cq9q2t5';
+
+// mongoose options
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+    if (err) {
+        console.log('Could not connect to database');
+        throw err;
+    }
+});
+
+// pull in our routes
+const router = require('./router.js');
+
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.use(compression());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+app.engine('handlebars', expressHandlebars({
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../views`);
+app.use(cookieParser());
+
+router(app);
+
+app.listen(port, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
+});
